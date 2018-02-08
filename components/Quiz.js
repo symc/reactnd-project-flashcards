d9ba575278f36f5fffea386879b5289330a55581
symc <sina.yamac@gmail.com>
2018-02-07 19:47:08 -0500

    feat: Improvements in the Quiz view
    1) Use FlipCard to show question/answer. The user can click on the card
    to switch from question/answer to answer/question.
    2) Show which question is currently being asked and what is the total
    number of questions at the top

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 import { getDeck } from '../utils/api';
 import { setLocalNotification, clearLocalNotification } from '../utils/helpers';
+import FlipCard from 'react-native-flip-card';
+import { green } from '../utils/colors';
 
 export default class Quiz extends Component {
   state = {
@@ -47,7 +49,8 @@ export default class Quiz extends Component {
 
   render() {
     const questionNo = this.state.questionNo;
-    const currQuestion = (this.state.deck && (questionNo < this.state.deck.questions.length)) ?
+    const totalNumberOfCards = (this.state.deck) ? this.state.deck.questions.length : 0;
+    const currQuestion = (this.state.deck && (questionNo < totalNumberOfCards)) ?
       this.state.deck.questions[questionNo] : null;
     const deckTitle = this.props.navigation.state.params.deckTitle;
     const refresh = this.props.navigation.state.params.refresh;
@@ -55,45 +58,60 @@ export default class Quiz extends Component {
       const question = (currQuestion) ? currQuestion.question : '';
       const answer = (currQuestion) ? currQuestion.answer : '';
       return (
-        <View style={{flex: 1}}>
-          <Text>Question: {question}</Text>
-          <Text>Answer: {answer}</Text>
-          <Button
-            title={'Correct'}
-            onPress={() => {
-              const questionNo = this.state.questionNo;
-              const correctAnswers = this.state.correctAnswers;
-              this.setState({
-                correctAnswers: correctAnswers + 1,
-                questionNo: questionNo + 1
-              }
-            )}}
-          />
-          <Button
-            title={'Incorrect'}
-            onPress={() => {
-              const questionNo = this.state.questionNo;
-              this.setState({
-                questionNo: questionNo + 1
-              }
-            )}}
-          />
-          <Button 
-            title={'Abort Quiz'}
-            onPress={() => this.props.navigation.navigate(
-              'DeckDetail', {
-                deckTitle: deckTitle,
-                refresh: refresh
-              }
-            )}
-          />
+        <View style={styles.container}>
+          <Text style={styles.remainingQuestions}>
+            Question {questionNo+1} of {totalNumberOfCards}
+          </Text>
+          <FlipCard style={styles.flipCard}>
+            {/* Face side shows the question */}
+            <View style={styles.frontCard}>
+              <Text style={styles.questionAnswer}>{question}</Text>
+              <Text style={styles.flipToSee}>Click to see the answer</Text>
+            </View>
+            {/* Back side shows the answer*/}
+            <View style={styles.backCard}>
+              <Text style={styles.questionAnswer}>{answer}</Text>
+              <Text style={styles.flipToSee}>Click to see the question</Text>
+            </View>
+          </FlipCard>
+          <View style={styles.bottomContainer}>
+            <Button
+              title={'Correct'}
+              onPress={() => {
+                const questionNo = this.state.questionNo;
+                const correctAnswers = this.state.correctAnswers;
+                this.setState({
+                  correctAnswers: correctAnswers + 1,
+                  questionNo: questionNo + 1
+                }
+              )}}
+            />
+            <Button
+              title={'Incorrect'}
+              onPress={() => {
+                const questionNo = this.state.questionNo;
+                this.setState({
+                  questionNo: questionNo + 1
+                }
+              )}}
+            />
+            <Button 
+              title={'Abort Quiz'}
+              onPress={() => this.props.navigation.navigate(
+                'DeckDetail', {
+                  deckTitle: deckTitle,
+                  refresh: refresh
+                }
+              )}
+            />
+          </View>
         </View>
       );
     } else {
       const correctAnswers = this.state.correctAnswers;
       const ratio = (this.state.deck) ? 100.0 * correctAnswers/(this.state.deck.questions.length) : 0.0;
       return (
-        <View style={{flex: 1}}>
+        <View style={styles.container}>
           <Text>Number of questions answered correctly: {correctAnswers}</Text>
           <Text>Percentage of correct answers: {ratio}%</Text>
           <Button 
@@ -109,3 +127,38 @@ export default class Quiz extends Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  remainingQuestions: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 15,
+    marginBottom: 20,
+  },
+  flipCard: {
+    alignSelf: 'center',
+    borderWidth: 0
+  },
+  frontCard: {
+    backgroundColor: '#d3d3d3'
+  },
+  backCard: {
+    backgroundColor: '#32ff32',
+  },
+  questionAnswer: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  flipToSee: {
+    fontFamily: 'Courier',
+    fontSize: 20
+  },
+  bottomContainer: {
+    marginTop: 150
+  }
+})
